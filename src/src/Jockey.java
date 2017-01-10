4196529292d3cb2432964633eae32ad2c2dbc301
romain bizeul <romain.bizeul@gmail.com>
2017-01-10 14:10:24 +0100

    zeiusk

--- src/src/Jockey.java
+++ src/src/Jockey.java
@@ -3,10 +3,22 @@ package src;
 public class Jockey {
 	String name;
 	int age;
-
-
-	public String getName() {
+	
+	public String getN() {
 		return name;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
 
+	
 }
